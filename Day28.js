acd6f37f31b124dfc6334431d8099127d9759881
Gayathribai54 <gayathrithakur95@gmail.com>
2025-08-23 06:27:34 -0700

    Add files via upload

---
+++ Day28.js
@@ -0,0 +1,75 @@
+//day 28 challenge
+
+
+class node{
+    constructor(data){
+       this.data=data;
+       this.next=null;
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+    }
+    insertAtPosition(data,pos){
+        if(pos<1){
+            console.log("invalid position");
+            return;
+        }
+        let temp=this.head;
+        while(temp!==null){
+            if(temp.data===data){
+                console.log("duplicate entries not allowed");
+                return;
+            }
+            temp=temp.next;
+        }
+        const newNode=new node(data);
+        if(pos===1){
+            newNode.next=this.head;
+            this.head=newNode;
+            if(this.tail===null){
+                this.tail=newNode;
+            }
+            return;
+        }
+        let current=this.head;
+        let count=1;
+        while(current!==null&& count<pos-1){
+            current=current.next;
+            count++
+        }
+        if(current===null){
+            console.log("invalid position");
+            return;
+        }
+        newNode.next=current.next;
+        current.next=newNode;
+        if(newNode.next===null){
+            this.tail=newNode;
+        }
+    }
+    printList(){
+        let result="";
+        let temp=this.head;
+        while(temp!==null){
+            result+=temp.data+"->";
+            temp=temp.next;
+        }
+        result+="null";
+        console.log(result);
+    }
+}
+
+let list=new LinkedList();
+
+list.insertAtPosition(1,1);
+list.insertAtPosition(2, 2);   // 1 -> 2 -> null
+list.insertAtPosition(4, 3);   // 1 -> 2 -> 4 -> null
+list.insertAtPosition(5, 4);   // 1 -> 2 -> 4 -> 5 -> null
+list.printList();
+list.insertAtPosition(3, 3);   // 1 -> 2 -> 3 -> 4 -> 5 -> null
+list.insertAtPosition(5, 2);   // Duplicate Entry Not Allowed
+list.insertAtPosition(10, 10); // Invalid Position
+list.printList();
